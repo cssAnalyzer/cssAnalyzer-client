1afdf3cde355670856d3fca2a40a6adaa7f15fee
Soodev20 <soodev20@gmail.com>
2021-10-01 00:14:02 +0900

    Style: Numbers & fontUrl

---
+++ src/constants/fontUrl.js
@@ -0,0 +1,6 @@
+const FONTS = {
+  MEGRIM: "https://fonts.googleapis.com/css2?family=Megrim",
+  RALE_WAY_LIGHT: "https://fonts.googleapis.com/css2?family=Raleway:wght@300",
+};
+
+export default FONTS;
