b0831c523baa5db287acbf69f9310d1af03c0541
Soodev20 <soodev20@gmail.com>
2021-10-04 07:37:00 +0900

    Modify: Change vaiable type

--- src/api/search.js
+++ src/api/search.js
@@ -4,7 +4,7 @@ const serverURL = process.env.LOCAL_SERVER_URI;
 const axiosInstance = axios.create({ serverURL });
 
 async function postSearchResult(page, inputUrl) {
-  const res = await axiosInstance.post(`${page}`, inputUrl);
+  const res = await axiosInstance.post(page, inputUrl);
 
   if (res) {
     const { data } = res;
