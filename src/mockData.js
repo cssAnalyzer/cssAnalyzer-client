4accca505dc3e136f40de8657e746a9c08422023
Soodev20 <soodev20@gmail.com>
2021-10-07 05:26:07 +0900

    Add: mock data (tag)

---
+++ src/mockData.js
@@ -0,0 +1,127 @@
+const mockData = {
+  totalNum: 2446,
+  filteredData: [
+    {
+      name: "HTML",
+      radius: 1,
+    },
+    {
+      name: "META",
+      radius: 15,
+    },
+    {
+      name: "LINK",
+      radius: 6,
+    },
+    {
+      name: "SCRIPT",
+      radius: 12,
+    },
+    {
+      name: "DIV",
+      radius: 458,
+    },
+    {
+      name: "A",
+      radius: 724,
+    },
+    {
+      name: "STYLE",
+      radius: 1,
+    },
+    {
+      name: "I",
+      radius: 117,
+    },
+    {
+      name: "SPAN",
+      radius: 340,
+    },
+    {
+      name: "IMG",
+      radius: 164,
+    },
+    {
+      name: "BUTTON",
+      radius: 4,
+    },
+    {
+      name: "H1",
+      radius: 1,
+    },
+    {
+      name: "FORM",
+      radius: 1,
+    },
+    {
+      name: "LEGEND",
+      radius: 1,
+    },
+    {
+      name: "SELECT",
+      radius: 1,
+    },
+    {
+      name: "OPTION",
+      radius: 33,
+    },
+    {
+      name: "OPTGROUP",
+      radius: 2,
+    },
+    {
+      name: "INPUT",
+      radius: 62,
+    },
+    {
+      name: "STRONG",
+      radius: 75,
+    },
+    {
+      name: "UL",
+      radius: 25,
+    },
+    {
+      name: "LI",
+      radius: 121,
+    },
+    {
+      name: "EM",
+      radius: 64,
+    },
+    {
+      name: "P",
+      radius: 75,
+    },
+    {
+      name: "DL",
+      radius: 14,
+    },
+    {
+      name: "DT",
+      radius: 14,
+    },
+    {
+      name: "DD",
+      radius: 99,
+    },
+    {
+      name: "IFRAME",
+      radius: 6,
+    },
+    {
+      name: "H2",
+      radius: 4,
+    },
+    {
+      name: "H3",
+      radius: 5,
+    },
+    {
+      name: "ADDRESS",
+      radius: 1,
+    },
+  ],
+}
+
+export default mockData;
