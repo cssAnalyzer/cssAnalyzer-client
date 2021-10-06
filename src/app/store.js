a27f9c49bdc3aaa815bd449385ec912e5b3b9ec5
Soodev20 <soodev20@gmail.com>
2021-10-07 05:24:24 +0900

    Add: Redux, Redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import { combineReducers } from "redux";
 import dataSlice from "../features/dataSlice";
+import errorSlice from "../features/errorSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
-export const store = configureStore({
-  reducer: {
-    data: dataSlice,
-  },
+const rootReducer = combineReducers({
+  data: dataSlice,
+  error: errorSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistReducers = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistReducers,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
+
+export const persistor = persistStore(store);
+export default store;
