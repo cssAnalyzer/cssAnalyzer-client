293f8cb08980e5927e860eda9f4576fb84b78a5f
Soodev20 <soodev20@gmail.com>
2021-10-10 01:53:14 +0900

    Add: Tooltips

--- src/components/graph/BubbleGraph.jsx
+++ src/components/graph/BubbleGraph.jsx
@@ -7,19 +7,19 @@ const Canvas = styled.svg`
   margin: 0 auto 0 auto;
   position: relative;
   text-align: center;
-  width: 80%;
-  height: 70%;
+  width: 100%;
+  height: 60%;
   border: solid 3px black;
   z-index: -1;
 `;
 
-const width = 1000;
-const height = 800;
+const width = Number(window.innerWidth || document.body.clientWidth);
+const height = Number(window.innerHeight || document.body.clientHeight);
 
 function BubbleGraph({ inputData, option }) {
   const svgRef = useRef();
   const svg = d3.select(svgRef.current);
-  const circleColor = (option === "/Attributes") ? "orange" : "green";
+  const circleColor = (option === "/attributes") ? "orange" : "green";
 
   useEffect(() => {
     svg.selectAll("g").remove();
@@ -29,19 +29,28 @@ function BubbleGraph({ inputData, option }) {
       .domain([d3.min(data, d => d.radius), d3.max(data, d => d.radius)])
       .range([10, 100]);
 
-    const circles = svg
-      .append("g")
+    const node = svg
       .selectAll("g")
       .data(data, d => d.name)
       .enter()
-      .append("circle")
+      .append("g")
+      .attr("class", "node")
+      .attr("transform", d => "translate(" + [width / 2, height / 3.5] + ")")
+
+    const circle = node.append("circle")
       .attr("r", d => scaleRadius(d.radius))
-      .attr("transform", "translate(" + [width / 2, height / 2] + ")")
       .attr("fill", circleColor)
       .attr("visibility", "visible")
       .attr("display", "block")
       .attr("stroke", circleColor)
       .style("opacity", 0.8);
+
+    const names = node.append("text")
+      .html(d => d.name)
+      .attr("text-anchor", "middle")
+      .attr("font-size", d => `${scaleRadius(d.radius)}px`)
+      .attr("visibility", "visible")
+      .attr("color", "black");
 
     const tooltip = svg
       .append("g")
@@ -49,25 +58,25 @@ function BubbleGraph({ inputData, option }) {
       .append("rect")
       .attr("class", "tooltip")
       .attr("fill", "white")
-      .attr("width", "100px")
-      .attr("height", "50px")
       .attr("text-align", "center")
       .style("opacity", 0);
 
-    circles.on("mouseover", (event, d) => {
+    node.on("mouseover", (event, d) => {
       const [x, y] = d3.pointer(event);
 
       tooltip.style("opacity", 0.8)
         .attr("stroke", "orange")
+        .attr("width", "100px")
+        .attr("height", "50px")
         .style("visibility", "visible")
-        .attr("transform", `translate(${x + width / 2}, ${y + height / 2})`);
+        .attr("transform", `translate(${x + width / 2}, ${y + height / 3.5})`);
 
       d3.select("#tooltipGroup")
         .append("text")
         .attr("id", "tooltipText")
         .attr("color", "black")
-        .attr("transform", `translate(${(x + width / 2) + 20}, ${(y + height / 2) + 30})`)
-        .html(`${d.name}<br>${d.radius}`);
+        .attr("transform", `translate(${(x + width / 2) + 20}, ${(y + height / 3.5) + 30})`)
+        .html(`total: ${d.radius}`);
     })
       .on("mouseout", () => {
         tooltip.style("opacity", 0)
@@ -77,26 +86,27 @@ function BubbleGraph({ inputData, option }) {
       });
 
     const tickNode = () => {
-      circles
+      circle
         .attr("cx", d => d.x)
         .attr("cy", d => d.y);
+      names
+        .attr("x", d => d.x)
+        .attr("y", d => d.y);
     }
 
     const simulation = d3.forceSimulation(data)
       .force("charge", d3.forceManyBody()
-        .strength(d => -(scaleRadius(d.radius) * 7 )))
+        .strength(d => - (scaleRadius(d.radius) * 7 )))
       .force("x", d3.forceX())
       .force("y", d3.forceY())
       .velocityDecay(0.1)
-      .force("collide", d3.forceCollide(d => scaleRadius(d.radius) + 5));
+      .force("collide", d3.forceCollide(d => scaleRadius(d.radius) + 7));
 
     simulation.nodes(data).on("tick", tickNode);
   }, [inputData]);
 
   return (
-    <>
-      <Canvas ref={svgRef}></Canvas>
-    </>
+    <Canvas ref={svgRef} />
   );
 }
 
