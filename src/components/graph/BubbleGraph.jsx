833c813545b8901cc8a2b6c9c2499bb6d1884a89
Soodev20 <soodev20@gmail.com>
2021-10-09 20:38:52 +0900

    FEAT: Bubble graph

--- src/components/graph/BubbleGraph.jsx
+++ src/components/graph/BubbleGraph.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
-import { useLocation } from "react-router";
+import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 import * as d3 from "d3";
 import PropTypes from "prop-types";
@@ -10,14 +9,25 @@ const Canvas = styled.svg`
   text-align: center;
   width: 80%;
   height: 70%;
+  border: solid 3px black;
+  z-index: -1;
 `;
 
-function BubbleGraph({ data, option }) {
+const width = 1000;
+const height = 800;
+
+function BubbleGraph({ inputData, option }) {
   const svgRef = useRef();
   const svg = d3.select(svgRef.current);
+  const circleColor = (option === "/Attributes") ? "orange" : "green";
 
   useEffect(() => {
-    svg.selectAll("circle").remove();
+    svg.selectAll("g").remove();
+    const data = inputData;
+
+    const scaleRadius = d3.scaleLinear()
+      .domain([d3.min(data, d => d.radius), d3.max(data, d => d.radius)])
+      .range([10, 100]);
 
     const circles = svg
       .append("g")
@@ -25,9 +35,46 @@ function BubbleGraph({ data, option }) {
       .data(data, d => d.name)
       .enter()
       .append("circle")
-      .attr("r", ({ radius }) => radius * 4)
-      .attr("fill", "orange")
-      .attr("stroke", "black");
+      .attr("r", d => scaleRadius(d.radius))
+      .attr("transform", "translate(" + [width / 2, height / 2] + ")")
+      .attr("fill", circleColor)
+      .attr("visibility", "visible")
+      .attr("display", "block")
+      .attr("stroke", circleColor)
+      .style("opacity", 0.8);
+
+    const tooltip = svg
+      .append("g")
+      .attr("id", "tooltipGroup")
+      .append("rect")
+      .attr("class", "tooltip")
+      .attr("fill", "white")
+      .attr("width", "100px")
+      .attr("height", "50px")
+      .attr("text-align", "center")
+      .style("opacity", 0);
+
+    circles.on("mouseover", (event, d) => {
+      const [x, y] = d3.pointer(event);
+
+      tooltip.style("opacity", 0.8)
+        .attr("stroke", "orange")
+        .style("visibility", "visible")
+        .attr("transform", `translate(${x + width / 2}, ${y + height / 2})`);
+
+      d3.select("#tooltipGroup")
+        .append("text")
+        .attr("id", "tooltipText")
+        .attr("color", "black")
+        .attr("transform", `translate(${(x + width / 2) + 20}, ${(y + height / 2) + 30})`)
+        .html(`${d.name}<br>${d.radius}`);
+    })
+      .on("mouseout", () => {
+        tooltip.style("opacity", 0)
+          .style("visibility", "hidden");
+
+        d3.select("#tooltipText").remove();
+      });
 
     const tickNode = () => {
       circles
@@ -36,12 +83,15 @@ function BubbleGraph({ data, option }) {
     }
 
     const simulation = d3.forceSimulation(data)
-      .force("charge", d3.forceManyBody().strength(300))
-      .force("collide", d3.forceCollide(d => d.radius * 4))
-      .force("center", d3.forceCenter(300, 500));
+      .force("charge", d3.forceManyBody()
+        .strength(d => -(scaleRadius(d.radius) * 7 )))
+      .force("x", d3.forceX())
+      .force("y", d3.forceY())
+      .velocityDecay(0.1)
+      .force("collide", d3.forceCollide(d => scaleRadius(d.radius) + 5));
 
     simulation.nodes(data).on("tick", tickNode);
-  }, []);
+  }, [inputData]);
 
   return (
     <>
@@ -51,7 +101,7 @@ function BubbleGraph({ data, option }) {
 }
 
 BubbleGraph.propTypes = {
-  data: PropTypes.array,
+  inputData: PropTypes.array,
   option: PropTypes.string,
 };
 
