cad1fa792297d3bd5fa4ae71e1f232694cd4325b
Soodev20 <soodev20@gmail.com>
2021-10-09 20:09:29 +0900

    Modify: new api, change variable names

--- src/pages/Tags.jsx
+++ src/pages/Tags.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
-import getSearchResult from "../api/search";
-import BubbleGraphTag from "../components/graph/BubbleGraphTag";
+import getSearchResult from "../api/getSearchResult";
+import BubbleGraph from "../components/graph/BubbleGraph";
 import GroupSelector from "../components/GroupSelector";
 import mockData from "../mockData";
+import Loading from "../components/graph/Loading";
 
 const Wrapper = styled.div`
   flex-direction: column;
@@ -17,34 +18,49 @@ const Wrapper = styled.div`
   height: 100vh;
 `;
 
-function Attributes() {
+function Tags() {
   const [groupView, setGroupView] = useState("all");
   const { pathname } = useLocation();
-  const [searchResult, setSearchResult] = useState({});
+  const [searchResult, setSearchResult] = useState([mockData.filteredData]);
+  const [isLoading, setIsLoading] = useState(false);
   const inputUrl = useSelector(state => state.data.inputUrl);
 
   const handleClick = ({ target }) => {
     setGroupView(target.value);
   }
 
+  async function getResult(pathname, inputUrl) {
+    setIsLoading(true);
+    const result = await getSearchResult(pathname, inputUrl);
+
+    if (!result) {
+      setSearchResult(mockData.filteredData);
+
+      return;
+    }
+    setSearchResult(result.filteredData);
+  }
+
   useEffect(() => {
-    const result = getSearchResult(pathname, inputUrl);
-    result ? setSearchResult(result) : setSearchResult(mockData.filteredData);
+    getResult(pathname, inputUrl);
+    setIsLoading(false);
   }, [])
 
   return (
     <>
       <Wrapper>
-        <BubbleGraphTag
-          data={searchResult}
-          option={groupView}
+        {isLoading && <Loading />}
+        <BubbleGraph
+          inputData={searchResult}
+          option={pathname}
         />
         <GroupSelector
           handleClick={handleClick}
         />
+        <div>{inputUrl}</div>
       </Wrapper>
     </>
   );
 }
 
-export default Attributes;
+export default Tags;
