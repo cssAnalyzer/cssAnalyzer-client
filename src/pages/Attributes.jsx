c0db44ecbae36cc2de13b608b1af932c2c42cebb
Soodev20 <soodev20@gmail.com>
2021-10-11 15:39:33 +0900

    FEAT: Attributes page

--- src/pages/Attributes.jsx
+++ src/pages/Attributes.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import getSearchResult from "../api/getSearchResult";
 import BubbleGraph from "../components/graph/BubbleGraph";
 import GroupSelector from "../components/GroupSelector";
+import mockData from "../mockData";
+import Loading from "../components/graph/Loading";
 
 const Wrapper = styled.div`
   flex-direction: column;
@@ -15,27 +19,42 @@ const Wrapper = styled.div`
 `;
 
 function Attributes() {
-  const [groupView, setGroupView] = useState("all");
+  const [groupView, setGroupView] = useState("bubble");
+  const { pathname } = useLocation();
+  const [searchResult, setSearchResult] = useState([mockData.filteredData]);
+  const [isLoading, setIsLoading] = useState(false);
+  const inputUrl = useSelector(state => state.data.inputUrl);
 
   const handleClick = ({ target }) => {
     setGroupView(target.value);
   }
 
-  const data = [
-    { name: "A", radius: 27, type: "div" },
-    { name: "B", radius: 40, type: "div" },
-    { name: "C", radius: 35, type: "p" },
-    { name: "D", radius: 10, type: "div" },
-    { name: "E", radius: 17, type: "p" },
-    { name: "F", radius: 17, type: "div" },
-  ];
+  async function getResult(pathname, inputUrl) {
+    setIsLoading(true);
+    const result = await getSearchResult(pathname, inputUrl);
+
+    if (!result) {
+      setSearchResult(mockData.filteredData);
+      setIsLoading(false);
+
+      return;
+    }
+    setSearchResult(result.filteredData);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getResult(pathname, inputUrl);
+  }, [])
 
   return (
     <>
       <Wrapper>
+        {isLoading && <Loading />}
         <BubbleGraph
-          data={data}
-          option={groupView}
+          data={searchResult}
+          title={inputUrl}
+          option={pathname}
         />
         <GroupSelector
           handleClick={handleClick}
